15c96b4cc4896266db03c7dce46c677df12c6c85
souhardyasarkar <68432043+souhardyasarkar@users.noreply.github.com>
2026-05-10 18:36:17 +0530

    Create Autoloader_test_file_ingestion.py

---
+++ Autoloader_test_file_ingestion.py
@@ -0,0 +1,95 @@
+######## Importing Libraries ###########
+
+from pyspark.sql.functions import col, to_timestamp, current_timestamp, try_to_timestamp
+from pyspark.sql import functions as F
+from pyspark.sql.functions import to_date, col, trim
+
+######## Calling DBUtils ###########
+
+dbutils.widgets.text("ref_id", "")
+dbutils.widgets.text("config_catalog", "")
+dbutils.widgets.text("config_schema", "")
+dbutils.widgets.text("config_table", "")
+# Step 1: Accept ref_id as a job parameter
+ref_id_str = dbutils.widgets.get("ref_id")
+config_catalog = dbutils.widgets.get("config_catalog")
+config_schema = dbutils.widgets.get("config_schema")
+config_table = dbutils.widgets.get("config_table")
+
+######## Calling Ref_id from Config Table ###########
+
+if not ref_id_str.isdigit():
+    raise ValueError("Invalid or missing ref_id. Please pass a numeric ref_id as a parameter.")
+
+ref_id = int(ref_id_str)
+
+# Step 2: Fetch configuration from the config table
+config_df = spark.table(f"{config_catalog}.{config_schema}.{config_table}").filter(f"ref_id = {ref_id}")
+
+if config_df.count() == 0:
+    raise ValueError(f"No configuration found for ref_id = {ref_id}")
+
+######## Autoloader Read Stream ###########
+
+config = config_df.collect()[0]
+input_path = config["input_path"]
+file_format = config["file_format"]
+schema_path = config["schema_path"]
+#archive_path = config["archive_path"]
+checkpoint_path = config["checkpoint_path"]
+table_catalog = config["table_catalog"]
+table_schema = config["table_schema"]
+ingst_tbl_name = config["ingst_tbl_name"]
+glob_filter = config["glob_filter"]
+
+# Step 3: Configure and run Auto Loader
+df = (
+    spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", file_format)
+        .option("header", "true")
+        .option("cloudFiles.includeExistingFiles", "true")
+        .option("cloudFiles.allowOverwrites", "true")
+        .option("delimiter", "\t")
+        .option("cloudFiles.schemaLocation", schema_path)
+        .option("cloudFiles.validateOptions", "false")
+        .option("pathGlobFilter", glob_filter)
+        .load(input_path)
+)
+
+######## Droping Unwanted Columns ###########
+
+df = df.drop(
+    "Total User Count",
+    "Active User Count",
+    "Inactive User Count",
+    "Division GUID"
+)
+
+######## Mapping Columns from source to target ###########
+
+df_new = (
+    df.withColumnRenamed("User Division ID", "division_id")
+      .withColumnRenamed("User Division ", "division_name")
+      .withColumnRenamed("Division Parent ID", "parent_division_id")
+      .withColumnRenamed("Division Parent ", "parent_division_name")
+      .withColumn("update_ts", current_timestamp())
+)
+
+######## Filtering ###########
+
+df_new = df_new.filter(col("division_id").isNotNull())
+
+######## Truncate Table (Optional) ###########
+
+spark.sql(f"TRUNCATE TABLE {table_catalog}.{table_schema}.{ingst_tbl_name}")
+
+######## Autoloader Write Stream ###########
+
+df_new.writeStream \
+    .format("delta") \
+    .option("checkpointLocation", checkpoint_path) \
+    .option("mergeSchema", "true") \
+    .trigger(availableNow=True) \
+    .outputMode("append") \
+    .table(f"{table_catalog}.{table_schema}.{ingst_tbl_name}")
